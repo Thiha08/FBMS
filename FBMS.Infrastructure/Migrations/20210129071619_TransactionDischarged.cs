f98d50289436b1256f69243158092e00e77a7358
Thiha Kyaw Htin <thihakyawhtin.mm@gmail.com>
2021-01-29 13:47:08 +0630

    migration for discharged

---
+++ FBMS.Infrastructure/Migrations/20210129071619_TransactionDischarged.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FBMS.Infrastructure.Migrations
+{
+    public partial class TransactionDischarged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DischargedDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDischarged",
+                table: "Transactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DischargedDate",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDischarged",
+                table: "Transactions");
+        }
+    }
+}
