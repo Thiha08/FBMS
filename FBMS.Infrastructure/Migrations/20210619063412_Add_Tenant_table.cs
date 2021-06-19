406380dfc6807c9b7f1c24b0d109f9ad17d9dfa8
Thiha <thihakyawhtin.mm@gmail.com>
2021-06-19 13:04:50 +0630

    migrate Tenant table

---
+++ FBMS.Infrastructure/Migrations/20210619063412_Add_Tenant_table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FBMS.Infrastructure.Migrations
+{
+    public partial class Add_Tenant_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HostUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HostPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tenants");
+        }
+    }
+}
